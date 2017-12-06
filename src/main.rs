14772a2f65f638082e8beb198689896315492b6e
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2017-12-06 21:02:23 +0100

    Add JSON endpoint

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 
-use rocket::response::content;
+use rocket_contrib::{Json, Value};
+use rocket::http::RawStr;
 
-#[get("/")]
-fn json() -> content::Json<&'static str> {
-    content::Json("{ 'hi': 'world' }")
+#[get("/<name>")]
+fn name(name: &RawStr) -> Option<Json<Value>> {
+    let response = format!("Hi there, {}!", name.as_str());
+    Some(Json(json!({ "status": response })))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![json]).launch();
+    rocket::ignite().mount("/", routes![name]).launch();
 }
